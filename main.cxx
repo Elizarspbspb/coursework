4be53dc8c79e89d613fbd2d2671971a2f985f5ff
Elizarspbspb <elizarspb@yandex.ru>
2020-03-31 18:44:49 +0300

    Добавлены искючения при работе с башней

--- main.cxx
+++ main.cxx
@@ -7,15 +7,19 @@
 #include <vector>
 #include <windows.h>
 #include "Ring.h"
-using namespace std;
 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <exception> 
+using namespace std;
+#define ERROR_400 400
 int main()
 {
 	int one = 0;
 	cout << "Enter the number of disks: ";
 	cin >> one;
 	Ring ring(one);
-
 	one = 0;
 	int from = 0; // с какой башни
 	int to = 0; // на какую 
@@ -32,35 +36,85 @@ int main()
 		cout << "'Steps' = " << c << endl;
 		i++;
 		ring.showTowers();  // прорисовка башен
-		ring.draw(0, 10); // перекидывает курсор запроса ввода вывода
-		cout << "From: "; // откуда переносим
-		cin >> from; 
-		cout << "To: "; // куда переносим
-		cin >> to;
-		if ((from == 1 || from == 2 || from == 3) || (to == 1 || to == 2 || to == 3)) 
+		ring.draw(0, 5); // перекидывает курсор запроса ввода вывода
+		try
 		{
-			c++;
-			if (!ring.shiftDisk(from, to, c)) // если не удалось переместить диск
+			cout << "From: "; // откуда переносим
+			cin >> from;
+			if (cin.fail())
 			{
-				cout << "/ ERROR \ - wrong size " << endl;
-				c--;
-				system("pause");
+				cin.clear();
+				while (cin.get() != '\n');
+				throw 2;
 			}
-			if (ring.win())
+
+			cout << "To: "; // куда переносим
+			cin >> to;
+			if (cin.fail())
 			{
-				system("cls");
-				ring.showTowers();
-				ring.draw(55, 12); // перекидывает курсор
-				cout << "! WIN !" << endl;
-				cout << "'Steps' = " << c << endl;
+				cin.clear();
+				while (cin.get() != '\n');
+				throw 3;
+			}
+			if ((from == 1 || from == 2 || from == 3) && (to == 1 || to == 2 || to == 3))
+			{
+				c++;
+				if (!ring.shiftDisk(from, to, c)) // если не удалось переместить диск
+				{
+					cout << "/ ERROR \ - wrong size " << endl;
+					c--;
+					system("pause");
+				}
+				if (ring.win())
+				{
+					system("cls");
+					try
+					{
+						ring.showTowers();
+					}
+					catch (int error)
+					{
+						switch (error)
+						{
+						case ERROR_400:
+							cout << endl << "\n Башни не найдены." << endl;
+							break;
+						default:
+							cout << endl << "\n Неизвестная ошибка." << endl;
+						}
+					}
+					try
+					{
+						ring.draw(55, 12); // перекидывает курсор
+					}
+					catch (int error)
+					{
+						switch (error)
+						{
+						case ERROR_400:
+							cout << endl << "\n Передвинуть курсоср невозможно." << endl;
+							break;
+						default:
+							cout << endl << "\n Неизвестная ошибка." << endl;
+						}
+					}
+					cout << "! WIN !" << endl;
+					cout << "'Steps' = " << c << endl;
+					break;
+				}
+			}
+			else if (from == 9 || to == 9)
 				break;
-			}
+			else
+				throw 1;
 		}
-		else if (from == 9 || to == 9)
-			break;
-		else
-		{
-			cout << "!  Invalid input !" << endl;
+		catch (int i) 
+		{ 
+			cout << "Error # " << i << endl; 
+			cout << "1 - Error, non-existent pyramid" << endl;
+			cout << "2 - Error, Incorrect input FROM " << endl;
+			cout << "3 - Error, Incorrect input TO " << endl;
+			cin.clear();
 			system("pause");
 		}
 	}
